1ef2df8539dfbf2d43aad553ebbd0d59bfcae62a
korattirth <84773402+korattirth@users.noreply.github.com>
2022-01-28 18:59:58 +0530

    AddDotNet File

---
+++ Helperland/WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿
+    const nav=document.querySelector('.nav_bar')
+    window.addEventListener('scroll', fixnav)
+    function fixnav(){
+            if(window.scrollY>nav.offsetHeight+50){
+        nav.classList.add('active')
+    }
+    else{
+        nav.classList.remove('active')
+    }
+        }
